e875a7679a00f2de8725670c40ce389fd61b110d
Sreejith <sreejiths@focaloid.com>
2023-01-12 10:16:49 +0530

    Space removed

--- src/scripts/xray.sh
+++ src/scripts/xray.sh
@@ -1,20 +1,26 @@
 #!/bin/bash
 #Fetching image details
-echo "Fetching Details for" : "${PARAM_IMAGE}"
-apiDomain="https://platform.slim.dev"
-imageDetails=$(curl -u ":${SAAS_KEY}" -X "GET" \
-  "${apiDomain}/orgs/${ORG_ID}/collection/images?limit=10&entity=${PARAM_IMAGE}" \
-  -H "accept: application/json")
+# echo "Fetching Details for" : "${PARAM_IMAGE}"
+
+# imageDetails=$(curl -u ":${SAAS_KEY}" -X "GET" \
+#   "${API_DOMAIN}/orgs/${ORG_ID}/collection/images?limit=10&entity=${PARAM_IMAGE}" \
+#   -H "accept: application/json")
  
 
-imageDetail=$(jq -r '.data[0]' <<< "${imageDetails}")
+# imageDetail=$(jq -r '.data[0]' <<< "${imageDetails}")
 
-connectorId=$(jq -r '.connector' <<< "${imageDetail}")
-nameSpace=$(jq -r '.namespace' <<< "${imageDetail}")
-imageId=$(jq -r '.id' <<< "${imageDetail}")
-entity=$(jq -r '.entity' <<< "${imageDetail}")
+# connectorId=$(jq -r '.connector' <<< "${imageDetail}")
+# nameSpace=$(jq -r '.namespace' <<< "${imageDetail}")
+# imageId=$(jq -r '.id' <<< "${imageDetail}")
+# entity=$(jq -r '.entity' <<< "${imageDetail}")
 
-echo "${imageId}"
+# echo "${imageId}"
+
+connectorId="${IMAGE_CONNECTOR}"
+nameSpace="${IMAGE_NAMESPACE}"
+tag="${IMAGE_TAG}"
+entity="${PARAM_IMAGE}"
+apiDomain="https://platform.slim.dev"
 
 
 echo Starting X-Ray Scan : "${PARAM_IMAGE}"
@@ -25,6 +31,7 @@ jsonDataUpdated=${jsonData//__CONNECTOR_ID__/${connectorId}}
 jsonDataUpdated=${jsonDataUpdated//__NAMESPACE__/${nameSpace}}
 jsonDataUpdated=${jsonDataUpdated//__REPO__/"${PARAM_IMAGE}"}
 jsonDataUpdated=${jsonDataUpdated//__COMMAND__/${command}}
+jsonDataUpdated=${jsonDataUpdated//__TAG__/${tag}}
 #Starting Xray Scan
 xrayRequest=$(curl -u ":${SAAS_KEY}" -X 'POST' \
   "${apiDomain}/orgs/${ORG_ID}/engine/executions" \
@@ -60,21 +67,20 @@ echo "${xrayReport}" >> /tmp/artifact-xray;#Uploading report to Artifact
 
 
 
-shaId=$(jq -r '.source_image.identity.digests[0]' <<< "${xrayReport}")
+#shaId=$(jq -r '.source_image.identity.digests[0]' <<< "${xrayReport}")
 #tag=$(jq -r '.source_image.identity.tags[0]' <<< "${xrayReport}")
 
-targetRef=$(jq -r '.target_reference' <<< "${xrayReport}")
-targ1=$(echo "${targetRef}" | cut -d "@" -f1)
-tag=$(echo "${targ1}" | cut -d ":" -f2)
 
-echo "${shaId}"
-echo "${tag}"
-echo "${imageId}"
+
+
+# echo "${shaId}"
+# echo "${tag}"
+
 
 
 
 #Adding the container to Favourites
 
-curl -u ":${SAAS_KEY}" -X POST "${apiDomain}/orgs/${ORG_ID}/collections/${FAV_COLLECTION_ID}/images/${imageId}/pins" -H  "accept: application/json" -H  "Content-Type: application/json" -d "{\"scope\":\"digest\",\"connector\":\"${connectorId}\",\"entity\":\"${entity}\",\"namespace\":\"${nameSpace}\",\"version\":\"${tag}\",\"digest\":\"${shaId}\",\"os\":\"linux\",\"arch\":\"amd64\"}"
+curl -u ":${SAAS_KEY}" -X POST "${apiDomain}/orgs/${ORG_ID}/collections/${FAV_COLLECTION_ID}/images//pins" -H  "accept: application/json" -H  "Content-Type: application/json" -d "{\"scope\":\"tag\",\"connector\":\"${connectorId}\",\"entity\":\"${entity}\",\"namespace\":\"${nameSpace}\",\"version\":\"${tag}\",\"digest\":\"\",\"os\":\"linux\",\"arch\":\"amd64\"}"
 
 
