3c7cd2a3f556ee1c58d6675dccb1b515cd4fefbb
Sreejith <sreejiths@focaloid.com>
2023-01-27 11:22:31 +0530

    Testing

--- src/scripts/xray.sh
+++ src/scripts/xray.sh
@@ -17,7 +17,7 @@ match=$(echo "${string}" | grep -oP '^(?:([^/]+)/)?(?:([^/]+)/)?([^@:/]+)(?:[@:]
 echo "${match}"
 IFS='/' 
 read -r -a parts <<< "$match"
-echo "${parts[1]}"
+echo "${parts[2]}"
 registry=${parts[0]}
 namespace=${parts[1]}
 repository=${parts[2]}
